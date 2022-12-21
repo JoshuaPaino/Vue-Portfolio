be95a92c58bb4573813cc5dc8d0a15f6075fe214
Joshua Paino <joshua.paino@gmail.com>
2022-12-21 09:43:07 -0600

    Finish Nav styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,6 +10,9 @@ import MaterialSymbolsMenu from "~icons/material-symbols/menu";
 import IcBaselineWbSunny from "~icons/ic/baseline-wb-sunny";
 import MaterialSymbolsModeNightRounded from "~icons/material-symbols/mode-night-rounded";
 import IcBaselineClose from "~icons/ic/baseline-close";
+import RiLinkedinFill from "~icons/ri/linkedin-fill";
+import RiGithubFill from "~icons/ri/github-fill";
+import RiTwitterFill from "~icons/ri/twitter-fill";
 </script>
 <template>
   <div class="fixed w-full h-[120px] shadow-xl z-[100] p-4">
@@ -57,21 +60,27 @@ import IcBaselineClose from "~icons/ic/baseline-close";
         <MaterialSymbolsMenu style="font-size: 1.8rem" @click="toggle()" />
         <button @click="toggleDark()" class="ml-6">
           <div v-if="isDark === true">
-            <MaterialSymbolsModeNightRounded style="font-size: 1.5rem" />
+            <MaterialSymbolsModeNightRounded
+              style="font-size: 1.5rem"
+              class="text-yellow-300"
+            />
           </div>
-          <div v-else><IcBaselineWbSunny style="font-size: 1.5rem" /></div>
+          <div v-else>
+            <IcBaselineWbSunny style="font-size: 1.5rem" />
+          </div>
         </button>
       </div>
       <!-- Mobile Menu End -->
     </div>
+    <!-- Add a Transition for this -->
     <div
-      class="fixed left-0 top-0 w-full h-screen dark:bg-black/30 bg-black/80 md:hidden"
+      class="fixed left-0 top-0 w-full h-screen dark:bg-black/67 bg-black/90 md:hidden"
       :style="{
         visibility: show ? 'visible' : 'hidden',
       }"
     >
       <div
-        class="md:hidden fixed left-0 top-0 w-[75%] sm:w-[65%] md:w-[45%] h-screen dark:bg-gray-600 bg-white p-6"
+        class="md:hidden fixed left-0 top-0 w-[75%] sm:w-[65%] md:w-[45%] h-screen dark:bg-slate-800 bg-white p-6"
       >
         <div class="flex w-full items-center justify-between">
           <img
@@ -82,7 +91,7 @@ import IcBaselineClose from "~icons/ic/baseline-close";
           />
           <div>
             <Button
-              class="rounded-full shadow-lg cursor-pointer shadow-gray-400 dark:shadow-black p-2"
+              class="rounded-full shadow-lg bg-gray-300 cursor-pointer shadow-gray-600 dark:shadow-black p-2"
               @click="toggle()"
             >
               <IcBaselineClose
@@ -92,10 +101,92 @@ import IcBaselineClose from "~icons/ic/baseline-close";
             </Button>
           </div>
         </div>
-        <p>lorem Ipsum</p>
-        <p>Value: {{ show ? "ON" : "OFF" }}</p>
+        <div class="border-b border-gray-300 my-4">
+          <p class="w-[85%] md:w-[90%] py-4">Let's build something together.</p>
+        </div>
+        <!-- Sidebar Menu -->
+        <div class="py-4 flex flex-col">
+          <ul class="uppercase">
+            <router-link to="/"
+              ><li class="py-4 text-lg">
+                <span class="border-blue-800 dark:border-white hover:border-b"
+                  >Home</span
+                >
+              </li></router-link
+            >
+            <router-link to="/"
+              ><li class="py-4 text-lg">
+                <span class="border-blue-800 dark:border-white hover:border-b"
+                  >Skills</span
+                >
+              </li></router-link
+            >
+            <router-link to="/"
+              ><li class="py-4 text-lg">
+                <span class="border-blue-800 dark:border-white hover:border-b"
+                  >Projects</span
+                >
+              </li></router-link
+            >
+            <router-link to="/"
+              ><li class="py-4 text-lg">
+                <span class="border-blue-800 dark:border-white hover:border-b"
+                  >Contact</span
+                >
+              </li></router-link
+            >
+          </ul>
+          <!-- Socials -->
+          <div class="pt-40">
+            <!-- Add a font color to this based on logo -->
+            <p class="uppercase tracking-widest">Let's Connect</p>
+            <div class="flex justify-around p-8">
+              <a
+                href="https://www.linkedin.com/"
+                target="_blank"
+                class="rounded-full shadow-lg bg-gray-300 cursor-pointer shadow-gray-600 dark:shadow-black p-2"
+              >
+                <RiLinkedinFill
+                  class="dark:text-blue-800 text-blue-800 text-2xl"
+                />
+              </a>
+
+              <a
+                href="https://www.github.com/"
+                target="_blank"
+                class="rounded-full shadow-lg bg-gray-300 cursor-pointer shadow-gray-600 dark:shadow-black p-2"
+              >
+                <RiGithubFill class="dark:text-black text-2xl" />
+              </a>
+
+              <a
+                href="https://www.twitter.com/"
+                target="_blank"
+                class="rounded-full shadow-lg bg-gray-300 cursor-pointer shadow-gray-600 dark:shadow-black p-2"
+              >
+                <RiTwitterFill
+                  class="dark:text-blue-600 text-blue-600 text-2xl"
+                />
+              </a>
+            </div>
+          </div>
+          <div class="flex justify-center items-center pt-20">
+            <button @click="toggleDark()">
+              <div v-if="isDark === true">
+                <MaterialSymbolsModeNightRounded
+                  class="text-yellow-300 text-4xl"
+                />
+              </div>
+              <div v-else>
+                <IcBaselineWbSunny class="text-4xl" />
+              </div>
+            </button>
+          </div>
+        </div>
+        <!-- Sidebar Menu -->
       </div>
     </div>
+    <!-- Transition for this -->
   </div>
 </template>
 <!--  -->
